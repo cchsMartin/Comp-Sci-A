d312292f296c82dee4cad2416561e615e489a1f2
NolanAttreed <70962299+NolanAttreed@users.noreply.github.com>
2020-10-13 22:14:37 -0400

    Add files via upload

---
+++ TenSidedDiceTest.java
@@ -0,0 +1,17 @@
+
+public class TenSidedDiceTest {
+
+	public static void main(String[] args) {
+		int count = 1;
+		TenSidedDice = new TenSidedDice(10);
+		roll.changeSections(10);
+		while(count < 2) {
+			roll.roll();
+			count++;
+			
+		}
+	}
+
+
+	}
+
